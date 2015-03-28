5cae64602bd5105ec9b2c81f4a3e61e0f17ed236
Lance Hudson <lance@clariturehealth.com>
2015-03-28 16:43:25 -0400

    it builds

---
+++ lance/wand/platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,11 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.megster.cordova.bluetoothserial/www/bluetoothSerial.js",
+        "id": "com.megster.cordova.bluetoothserial.bluetoothSerial",
+        "clobbers": [
+            "window.bluetoothSerial"
+        ]
+    }
+]
+});
