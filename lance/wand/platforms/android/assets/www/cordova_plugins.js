1a5d2361958800e6cc2b46fdb464d03183b8182b
Lance Hudson <lance@clariturehealth.com>
2015-03-28 20:22:18 -0400

    i hate yoeman sometimes

--- lance/wand/platforms/android/assets/www/cordova_plugins.js
+++ lance/wand/platforms/android/assets/www/cordova_plugins.js
@@ -1,11 +1,7 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/com.megster.cordova.bluetoothserial/www/bluetoothSerial.js",
-        "id": "com.megster.cordova.bluetoothserial.bluetoothSerial",
-        "clobbers": [
-            "window.bluetoothSerial"
-        ]
-    }
-]
+module.exports = [];
+module.exports.metadata = 
+// TOP OF METADATA
+{}
+// BOTTOM OF METADATA
 });
