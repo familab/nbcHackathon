41859e36c0eef442b8a92869217570666b7ba5b2
singlerider <haroboy876@gmail.com>
2015-03-28 22:20:53 -0400

    added ble scratch data advertisement

--- shane/WandGestures/WandGestures/WandGestures.ino
+++ shane/WandGestures/WandGestures/WandGestures.ino
@@ -24,6 +24,7 @@ void loop() {
   // Get the current acceleration with a conversion of 3.91×10-3 g/unit.
   AccelerationReading accel = Bean.getAcceleration();
   String stringToPrint = String();
+  String fl = String();
   stringToPrint = stringToPrint + "X: " + accel.xAxis + "\tY: " + accel.yAxis + "\tZ: " + accel.zAxis;
   // Find the difference between the current acceleration and that of 200ms ago.
   int accelDifference = getAccelDifference(previousAccel, accel); 
@@ -32,11 +33,26 @@ void loop() {
 
   // Check if the Bean has been moved beyond our threshold.
 
-  if (accel.zAxis > 500)
+  if (accel.zAxis > 500 || accel.zAxis < -500)
   {
     //if (accel.xAxis > 0 && accel.yAxis > 0){
     Bean.setLed(0,255,15);
     //Serial.print(stringToPrint);
+
+    uint8_t buffer[2];
+
+    uint16_t an0 = 0;
+    uint16_t an1 = 1;
+
+    buffer[0] = an0 & 0xFF;
+    buffer[1] = an0 >> 8;
+
+    Bean.setScratchData(1, buffer, 2);
+
+    buffer[0] = an1 & 0xFF;
+    buffer[1] = an1 >> 8;
+
+    Bean.setScratchData(2, buffer, 2);
     Serial.println("Flick!");
     Bean.sleep(2000);
     //}
@@ -45,12 +61,41 @@ void loop() {
   else if (accelDifference > THRESHOLD){   
     // Blink the LED
     Bean.setLed(50, 0, 0);
+
+    uint8_t buffer[2];
+
+    uint16_t an0 = 2;
+    uint16_t an1 = 3;
+
+    buffer[0] = an0 & 0xFF;
+    buffer[1] = an0 >> 8;
+
+    Bean.setScratchData(1, buffer, 2);
+
+    buffer[0] = an1 & 0xFF;
+    buffer[1] = an1 >> 8;
+
+
     Serial.println(stringToPrint);
     Bean.sleep(20);
   }
 
   else if (accelDifference <= THRESHOLD){
     Bean.setLed(1, 1, 1);
+    
+    uint8_t buffer[2];
+
+    uint16_t an0 = 4;
+    uint16_t an1 = 5;
+
+    buffer[0] = an0 & 0xFF;
+    buffer[1] = an0 >> 8;
+
+    Bean.setScratchData(1, buffer, 2);
+
+    buffer[0] = an1 & 0xFF;
+    buffer[1] = an1 >> 8;
+    
     Serial.println("Not moving");
     //Bean.sleep(50);
   }
@@ -64,13 +109,5 @@ int getAccelDifference(AccelerationReading readingOne, AccelerationReading readi
   // Return the magnitude
   return deltaX + deltaY + deltaZ;   
 }
-
-
-
-
-
-
-
-
 
 
